81498e99de7ce4124fdefc14f692a6e0278719e8
Toan <nguyentoan31072k3@gmail.com>
2022-11-24 23:47:28 +0700

    js

--- script.js
+++ script.js
@@ -192,22 +192,91 @@ headerObserver.observe(header)
 // Reveal sections
 const allSections = document.querySelectorAll('.section')
 
-const revealSection = function (entries, observe) {
+const revealSection = function (entries, observer) {
     const [entry] = entries
-    console.log(entry)
-
+    // console.log(entry)
     if (!entry.isIntersecting) return
 
     entry.target.classList.remove('section--hidden')
-    observe.unobserve(entry.target)
+    observer.unobserve(entry.target)
 }
 
 const sectionObserver = new IntersectionObserver(revealSection, {
     root: null,
-    threshold: 0.15,
+    threshold: 0.1,
 })
 
 allSections.forEach(function (section) {
+
     sectionObserver.observe(section)
     section.classList.add('section--hidden')
 })
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]')
+console.log(imgTargets)
+
+const loadImg = function (entries, observe) {
+    const [entry] = entries
+    console.log(entry)
+    if (!entry.isIntersecting) return
+
+    //Replace src with data-src
+    entry.target.src = entry.target.dataset.src
+
+    entry.target.addEventListener('load', function () {
+        entry.target.classList.remove('lazy-img')
+    })
+
+    observe.unobserve(entry.target)
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px'
+})
+
+imgTargets.forEach(function (img) {
+    imgObserver.observe(img)
+})
+
+// slider
+
+const slides = document.querySelectorAll('.slide')
+const btnLeft = document.querySelector('.slider__btn--left')
+const btnRight = document.querySelector('.slider__btn--right')
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+    slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`))
+}
+
+goToSlide(0);
+
+// Next slide
+const nextSlide = function () {
+    if (curSlide == maxSlide - 1) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+    goToSlide(curSlide)
+}
+
+// Prev Slide
+const prevSlide = function () {
+    if (curSlide == 0) {
+        curSlide = maxSlide - 1;
+    } else {
+        curSlide--;
+    }
+    goToSlide(curSlide)
+}
+
+btnRight.addEventListener('click', nextSlide)
+
+btnLeft.addEventListener('click', prevSlide)
+
